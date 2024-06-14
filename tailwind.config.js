eac4578b057b1e0e263291b008a27ba6f4cebb1e
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-06-14 16:15:27 +0200

    careate hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,29 @@
 module.exports = {
   content: ["./src/**/index.html"],
   theme: {
-    extend: {},
-    screens: {
-      taplet: "640px",
-      labtop: "1024px",
-      desktop: "1280px",
-      xs: "450px",
-      sm: { max: "670px" },
-      md: { min: "768px", max: "1023px" },
+    container: {
+      center: true,
+      padding: "18px",
     },
-    colors:{
-      teal:"red"
-    }
+    extend: {
+      backgroundImage: {
+        "bg-landing": "url('/1350469.png')",
+      },
+      // spacing: {
+      //   "010": "1px",
+      // },
+    },
+    // screens: {
+    //   taplet: "640px",
+    //   labtop: "1024px",
+    //   desktop: "1280px",
+    //   xs: "450px",
+    //   sm: { max: "670px" },
+    //   md: { min: "768px", max: "1023px" },
+    // },
+    // colors: {
+    //   teal: "red",
+    // },
   },
   plugins: [],
 };
